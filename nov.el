41e6a9cf142465633b7128a3b0e284ff6cb582d7
Vasilij Schneidermann <mail@vasilij.de>
2020-02-15 22:44:15 +0100

    Conform to org-link-store API

--- nov.el
+++ nov.el
@@ -816,14 +816,13 @@ Saving is only done if `nov-save-place-file' is set."
     (error "Invalid nov.el link")))
 
 (defun nov-org-link-store ()
-  (when (not (and (eq major-mode 'nov-mode) nov-file-name))
-    (error "Not in a nov.el buffer"))
-  (when (not (integerp nov-documents-index))
-    (setq nov-documents-index 0))
-  (org-store-link-props
-   :type "nov"
-   :link (format "nov:%s::%d:%d" nov-file-name nov-documents-index (point))
-   :description (format "EPUB file at %s" nov-file-name)))
+  (when (and (eq major-mode 'nov-mode) nov-file-name)
+    (when (not (integerp nov-documents-index))
+      (setq nov-documents-index 0))
+    (org-store-link-props
+     :type "nov"
+     :link (format "nov:%s::%d:%d" nov-file-name nov-documents-index (point))
+     :description (format "EPUB file at %s" nov-file-name))))
 
 (org-link-set-parameters
  "nov"
